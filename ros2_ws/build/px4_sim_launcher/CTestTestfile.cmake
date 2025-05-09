d4bb06a12874fceb1e18e320352f9e7ec76ab362
Stefano Bassino <s.bassino@dropla.tech>
2025-05-09 18:23:59 +0200

    Compiling the ros2_ws ann removing PX4-autopilot folder

---
+++ ros2_ws/build/px4_sim_launcher/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/ros2_ws/src/px4_sim_launcher
+# Build directory: /home/ubuntu/ros2_ws/build/px4_sim_launcher
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
