9a48def3f20eea714fa44bf8d29e80cbdcef5bf6
Stefano Bassino <s.bassino@dropla.tech>
2025-05-22 16:31:59 +0200

    Remove ROS 2 build artifacts from tracking

--- ros2_ws/build/camera_forwarder/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/prova_container/ros2_ws/src/camera_forwarder
-# Build directory: /workspaces/prova_container/ros2_ws/build/camera_forwarder
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros2_ws/build/orbslam3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/prova_container/ros2_ws/src/orbslam3_ros2
-# Build directory: /workspaces/prova_container/ros2_ws/build/orbslam3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
